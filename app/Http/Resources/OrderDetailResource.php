95e95e7c6fed284676a8a727e40eaff40cc6db36
MuhammadSagor <sagorwdpf@gmail.com>
2026-05-17 21:11:41 +0600

    update price

--- app/Http/Resources/OrderDetailResource.php
+++ app/Http/Resources/OrderDetailResource.php
@@ -20,6 +20,21 @@ class OrderDetailResource extends JsonResource
             'single_hand_punjabi' => $this->single_hand_punjabi,
             'double_hand_punjabi' => $this->double_hand_punjabi,
             'punjabi'             => $this->punjabi,
+            'fabric_qty'          => $this->fabric_qty,
+            'fabric_price'        => $this->fabric_price,
+            'labor_qty'           => $this->labor_qty,
+            'labor_price'         => $this->labor_price,
+            'design_qty'          => $this->design_qty,
+            'design_price'        => $this->design_price,
+            'button_qty'          => $this->button_qty,
+            'button_price'        => $this->button_price,
+            'embroidery_qty'      => $this->embroidery_qty,
+            'embroidery_price'    => $this->embroidery_price,
+            'courier_qty'         => $this->courier_qty,
+            'due'                 => $this->due,
+            'total'               => $this->total,
+            'advance'             => $this->advance,
+            'button_price'        => $this->button_price,
         ];
     }
 }
